078962348859043bca24dff2279a797b686d0f20
Eino Korte <e.i.korte@gmail.com>
2022-10-09 19:40:59 +0300

    added player

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,13 +11,15 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	
 	Globaldata.playerPosition = position
 	if not is_on_floor():
 		velocity.y -= weight * gravity * delta
+
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	var input = Vector3()
   
@@ -30,17 +32,22 @@ func _physics_process(delta):
 	if Input.is_action_pressed("d"):
 		input.x += 1
 	input = input.normalized()
-  
+	if (velocity.x < -0.1 || velocity.x > 0.1) || (velocity.z < -0.1 || velocity.z > 0.1):
+		$AnimatedSprite3d.playing = true
+		print(velocity.z)
+	else:
+		$AnimatedSprite3d.playing = false
+	
 	var dir = (transform.basis.z * input.z + transform.basis.x * input.x)
 	velocity.x = lerp(velocity.x, dir.x * SPEED, 0.2)
 	velocity.z = lerp(velocity.z, dir.z * SPEED, 0.2)
 	if Input.is_action_pressed("ui_left"):
-		rotation.y += 5 * delta
+		rotation.y += 3 * delta
 	if Input.is_action_pressed("ui_right"):
-		rotation.y -= 5 * delta
+		rotation.y -= 3 * delta
 	if Input.is_action_pressed("ui_up"):
 		pass
 	if Input.is_action_pressed("ui_down"):
 		pass
-	velocity = velocity.rotated(Vector3(0,1,0), angle)
 	move_and_slide()
+	
