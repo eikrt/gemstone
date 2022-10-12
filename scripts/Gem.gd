69e25beacbba2727866cd0e1d9134b03c3d57985
Eino Korte <e.i.korte@gmail.com>
2022-10-12 18:49:18 +0300

    added gems

---
+++ scripts/Gem.gd
@@ -0,0 +1,20 @@
+extends Area3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_gem_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_gem_body_entered(body):
+	Globaldata.playerGems += 1
+	queue_free()
--- scripts/globaldata.gd
+++ scripts/globaldata.gd
@@ -1,3 +1,4 @@
 extends Node
 var cameraTargetPosition = Vector3()
 var playerPosition = Vector3()
+var playerGems = 0
